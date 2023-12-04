93d52c9ffdce3ae52f62918b64b341a410d559e8
adnan-vyrill <adnan@vyrill.com>
2023-12-04 18:47:24 +0500

    add github version

--- src/App.js
+++ src/App.js
@@ -9,8 +9,6 @@ function App() {
       <header className="App-header">
         <h1>Test {version}</h1>
         {console.log("helloWorld")}
-        <h1>Test Version</h1>
-        dfv
       </header>
     </div>
   );
